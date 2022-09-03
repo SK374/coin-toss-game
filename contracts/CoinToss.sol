c20d7c70503ed97d05e5fd179c669fc9cec90eaa
SK374 <SK374@protonmail.com>
2022-09-03 20:17:04 +0200

    Changes to coin toss contract

--- contracts/CoinToss.sol
+++ contracts/CoinToss.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
@@ -21,7 +21,6 @@ contract CoinToss is VRFConsumerBase {
         uint256 betAmount;
     }
 
-    address public CoinTossCreator;
     mapping(address => status) public playerStatus;
     uint256 public random;
     uint256 private constant NOT_STARTED = 41;
@@ -32,7 +31,7 @@ contract CoinToss is VRFConsumerBase {
     event SidePickStarted(bytes32 indexed requestId);
     event SidePicked(bytes32 indexed requestId);
 
-    constructor(address creator)
+    constructor()
         VRFConsumerBase(
             0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
             0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
@@ -40,7 +39,6 @@ contract CoinToss is VRFConsumerBase {
     {
         keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
         fee = 0.1 * 10**18; // 0.1 LINK
-        CoinTossCreator = creator;
     }
 
     function flipCoin() public returns (bytes32 requestId) {
